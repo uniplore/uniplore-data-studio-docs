57c0e1dd9001d07cffa777cd1774c1b0dd3bca64
jy_RM_c <92495745+jyRMc@users.noreply.github.com>
2023-11-21 17:14:33 +0800

    delete: 删除无用文件

--- static/sample-file/udistudio/bestpractice/extract-load/etl_test_input.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE etl_test_input (
-  project_name varchar(255) DEFAULT NULL,
-  start_date date DEFAULT NULL,
-  end_date date DEFAULT NULL,
-  diff_date int(11) DEFAULT NULL,
-  performance varchar(255) DEFAULT NULL,
-  duration varchar(255) DEFAULT NULL,
-  message varchar(255) CHARACTER SET utf8 DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
