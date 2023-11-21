d272d79df22e2532b138a5aa93cee646ada11701
jy_RM_c <92495745+jyRMc@users.noreply.github.com>
2023-11-21 17:04:00 +0800

    merge: 合并代码

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\src\\css\\custom.css'),
+];
