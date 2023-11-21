1fcd5ac46d4a883dd972eb1b7f10ff648f2c9316
jy_RM_c <92495745+jyRMc@users.noreply.github.com>
2023-11-21 17:04:53 +0800

    Revert "merge: 合并代码"
    This reverts commit d272d79df22e2532b138a5aa93cee646ada11701.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
-  require('D:\\IdeaProjects\\uniplore-data-studio-docs\\src\\css\\custom.css'),
-];
